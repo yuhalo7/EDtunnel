bb2c331c742b2b57975de0070fab1ddd8501d203
SonzaiX シ <abhiedthea@gmail.com>
2024-04-27 11:52:12 +0700

    Proxy Digital Ocean

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa5';
 
-const proxyIPs = ['45.194.25.18'];
+const proxyIPs = ['68.183.180.154'];
 
 // if you want to use ipv6 or single proxyIP, please add comment at this line and remove comment at the next line
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
